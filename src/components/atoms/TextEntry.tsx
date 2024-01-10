f0600a5d412517fe2d00af61eca6f1146d8569d0
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-09 16:00:53 -0800

    add TextEntry component

---
+++ src/components/atoms/TextEntry.tsx
@@ -0,0 +1,22 @@
+const TextEntry = () => {
+
+  function handleSave(e) {
+    e.PreventDefault();
+    
+
+  }
+  
+  return (
+    <div className="text-form">
+      <form action="" className="text-entry">
+        <input type="text" placeholder="These are my thoughts today"/>
+        <input type="text" placeholder="Worms and other things"/>
+        <input type="date" />
+        <textarea name="entry" placeholder="Dear Diary, today I stuffed a bunch of olives into the DVD player and then I remembered that I don't have a DVD player." cols={100} rows={50} ></textarea>
+        <input type="submit" onClick={handleSave}></input>
+      </form>
+    </div>
+  );
+}
+
+export default TextEntry;
