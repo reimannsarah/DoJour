4c1c1c89c98971abe15beffb7a14e9f0e5c6f433
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-22 16:03:39 -0800

    WIP register component

---
+++ src/components/sub/Register.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <input type="submit" value="Register" />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
