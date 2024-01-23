7478f325a96f7838fff4d9d238d1e227f694884d
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-22 16:03:05 -0800

    move file

--- src/components/sub/TextEntry.tsx
+++ src/components/sub/TextEntry.tsx
@@ -1,10 +1,11 @@
 const TextEntry = () => {
 
-  function handleSave(e) {
-    e.PreventDefault();
+  // function handleSave(e) {
+  //   e.PreventDefault();
+
     
-    
-  }
+
+  // }
   
   return (
     <div className="text-form">
@@ -13,7 +14,7 @@ const TextEntry = () => {
         <input type="text" placeholder="Worms and other things"/>
         <input type="date" />
         <textarea name="entry" placeholder="Dear Diary, today I stuffed a bunch of olives into the DVD player and then I remembered that I don't have a DVD player." cols={100} rows={50} ></textarea>
-        <input type="submit" onClick={handleSave}></input>
+        <input type="submit"></input>
       </form>
     </div>
   );
