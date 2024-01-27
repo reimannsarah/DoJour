475434f46d290086d1e73085fb796b03f61b45e0
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-26 17:04:16 -0800

    add motion functions

---
+++ utils/motion.ts
@@ -0,0 +1,49 @@
+export function slideInFromLeft(delay: number) {
+  return {
+    hidden: { x: -100, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: delay,
+        duration: 0.5,
+      },
+    },
+  };
+}
+
+export function slideInFromRight(delay: number) {
+  return {
+    hidden: { x: 100, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: delay,
+        duration: 0.5,
+      },
+    },
+  };
+}
+
+export const slideInFromTop = (delay: number) => ({
+  hidden: { y: -100, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: { delay: delay, duration: 0.5 },
+  },
+});
+
+export function fadeIn(delay: number) {
+  return {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: delay,
+        duration: 0.5,
+      },
+    },
+  };
+}
