5e895d0279cd833497fffd02f6de7cbf40b9b803
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-26 23:41:53 -0800

    style landing page

--- src/components/main/Landing.tsx
+++ src/components/main/Landing.tsx
@@ -1,18 +1,23 @@
-import Login from '../sub/Login'
-import Register from '../sub/Register'
-import { User } from '../../service/apiService'
+import Login from "../sub/Login";
+import Register from "../sub/Register";
+import { User } from "../../service/apiService";
 
 interface LandingProps {
-  onLogin: (user: User) => void
+  onLogin: (user: User) => void;
 }
 
 const Landing = (props: LandingProps) => {
   return (
-    <div>
-      <Login onSubmit={props.onLogin}/>
-      <Register onSubmit={props.onLogin}/>
+    <div className="flex flex-col gap-10 mt-[150px]">
+      <div className="w-max my-0 mx-auto">
+        <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl text-white font-bold">
+          Dear diary...
+        </h1>
+      </div>
+      <Login onSubmit={props.onLogin} />
+      <Register onSubmit={props.onLogin} />
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
