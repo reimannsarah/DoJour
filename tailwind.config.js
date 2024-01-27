d37369934d681bbdec2665eaf927e40b3a4355e1
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-26 23:40:49 -0800

    add typewriter animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }
+        }
+      },
+      animation: {
+        typing: "typing 2s steps(20) infinite alternate, blink 1s infinite"
+      },
+      colors: {
+        'primary': '#051007',
+        'secondary': '#739A6B',
+      }
+    },
   },
   plugins: [],
 }
