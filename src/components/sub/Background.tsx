7ffccdbcaf3ff2a1ed51595bb38e9012968eeaf3
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-26 23:42:07 -0800

    add background component

---
+++ src/components/sub/Background.tsx
@@ -0,0 +1,21 @@
+
+const Background = () => {
+  return (
+    <div className="bg-primary -z-[200px]">
+			<ul className="circles h-full w-full">
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+		</div>
+  )
+}
+
+export default Background
