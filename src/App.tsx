9597b9459b021ae86d1890edfa799663a7e477ac
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-26 16:05:28 -0800

    add user auth and remove control

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import "./App.css";
-import Control from "./components/Control";
+import LandingPage from "./components/main/Landing";
+import { Dashboard } from "./components/main/Dashboard";
+import { UserContext } from "./components/main/UserContext";
+import { useState, useEffect } from "react";
+import { User } from "./service/apiService";
 
 function App() {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsLoggedIn(!!token);
+  }, []);
+
+  const handleLogin = (user: User) => {
+    setUser(user);
+    setIsLoggedIn(true);
+    if (user.token) {
+      localStorage.setItem("token", user.token);
+    }
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+    setIsLoggedIn(false);
+    localStorage.removeItem("token");
+  };
+
   return (
-    <>
-      <Control />
-    </>
+    <UserContext.Provider value={{ user, setUser, isLoggedIn, handleLogin, handleLogout }}>
+      {!isLoggedIn ? <LandingPage onLogin={handleLogin} /> : <Dashboard />}
+    </UserContext.Provider>
   );
 }
 
