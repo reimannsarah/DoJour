6a71a8c9b03b208b092bafc5834bbe29f06a8544
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-26 16:05:51 -0800

    add UserContext

---
+++ src/components/main/UserContext.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { User } from '../../service/apiService';
+
+type UserContextType = {
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+  isLoggedIn: boolean;
+  handleLogin: (user: User) => void;
+  handleLogout: () => void;
+};
+
+export const UserContext = React.createContext<UserContextType | null>(null);
