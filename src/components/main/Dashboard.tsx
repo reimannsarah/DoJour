372c1752ccbfabf9b8ae19fd7a388b724323fcf1
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-26 16:49:19 -0800

    add state to run useEffect in entries

--- src/components/main/Dashboard.tsx
+++ src/components/main/Dashboard.tsx
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import Entries from '../sub/Entries';
 import TextEntry from '../sub/TextEntry';
 import Button from '../sub/Button';
 
 export const Dashboard = () => {
+  const [lastSubmission, setLastSubmission] = useState<Date | null>(null);
+
+  const handleSubmission = () => {
+    setLastSubmission(new Date());
+  };
+
   return (
     <div>
-      <TextEntry />
-      <Entries />
+      <TextEntry onSubmission={handleSubmission} />
+      <Entries lastSubmission={lastSubmission} />
       <Button />
     </div>
-  )
-}
+  );
+};
