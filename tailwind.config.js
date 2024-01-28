07cd7773a8ee4a38458100c308da5475153276ca
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-27 17:15:33 -0800

    add font families

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,34 +3,37 @@ export default {
   content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      keyframes: {
-        typing: {
-          "0%": {
-            width: "0%",
-            visibility: "hidden"
+      fontFamily: {
+        'header': "Abril Fatface",
+        'body': "Comic Neue",
+      },
+        keyframes: {
+          typing: {
+            "0%": {
+              width: "0%",
+              visibility: "hidden",
+            },
+            "100%": {
+              width: "100%",
+            },
           },
-          "100%": {
-            width: "100%"
-          }
+          blink: {
+            "50%": {
+              borderColor: "transparent",
+            },
+            "100%": {
+              borderColor: "white",
+            },
+          },
         },
-        blink: {
-          "50%": {
-            borderColor: "transparent"
-          },
-          "100%": {
-            borderColor: "white"
-          }
-        }
+        animation: {
+          typing: "typing 2s steps(20) infinite alternate, blink 1s infinite",
+        },
+        colors: {
+          primary: "#051007",
+          secondary: "#739A6B",
+        },
       },
-      animation: {
-        typing: "typing 2s steps(20) infinite alternate, blink 1s infinite"
-      },
-      colors: {
-        'primary': '#051007',
-        'secondary': '#739A6B',
-      }
     },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+  }
