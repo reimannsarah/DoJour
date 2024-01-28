fe23109ab9ea90dad8b2c1b88b6449d5efda9bd3
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-28 14:24:44 -0800

    add update button and functionality

--- src/components/main/Dashboard.tsx
+++ src/components/main/Dashboard.tsx
@@ -3,7 +3,11 @@ import Entries from "../sub/Entries";
 import TextEntry from "../sub/TextEntry";
 import { UserContext } from "./UserContext";
 import Button from "../sub/Button";
-import { Entry, deleteEntry, getEntriesByUserId } from "../../service/apiService";
+import {
+  Entry,
+  deleteEntry,
+  getEntriesByUserId,
+} from "../../service/apiService";
 import Modal from "react-modal";
 import { XCircleIcon } from "@heroicons/react/16/solid";
 
@@ -12,6 +16,7 @@ export const Dashboard = () => {
   const [lastSubmission, setLastSubmission] = useState<Date | null>(null);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [currentEntry, setCurrentEntry] = useState<Entry | null>(null);
+  const [isEditing, setIsEditing] = useState(false);
 
   const handleSubmission = () => {
     setLastSubmission(new Date());
@@ -27,35 +32,48 @@ export const Dashboard = () => {
           setModalIsOpen={setModalIsOpen}
           setCurrentEntry={setCurrentEntry}
         />
-        <TextEntry
-          onSubmission={handleSubmission}
-          setModalIsOpen={setModalIsOpen}
-          modalIsOpen={modalIsOpen}
-          setCurrentEntry={setCurrentEntry}
-        />
+          <TextEntry
+            currentEntry={currentEntry}
+            setIsEditing={setIsEditing}
+            isEditing={isEditing}
+            onSubmission={handleSubmission}
+            setModalIsOpen={setModalIsOpen}
+            modalIsOpen={modalIsOpen}
+            setCurrentEntry={setCurrentEntry}
+          />
         <Modal
           isOpen={modalIsOpen}
           onRequestClose={() => setModalIsOpen(false)}
           className="relative bg-tertiary w-3/5 h-3/5 my-44 mx-auto rounded-lg p-10"
         >
-          <XCircleIcon className="absolute w-5 h-5 right-0 top-0 text-darkTertiary m-3 cursor-pointer" onClick={() => setModalIsOpen(false)}/>
+          <XCircleIcon
+            className="absolute w-5 h-5 right-0 top-0 text-darkTertiary m-3 cursor-pointer"
+            onClick={() => setModalIsOpen(false)}
+          />
           <div className="flex flex-row justify-between">
-            <h1 className="font-header text-5xl text-darkTertiary">{currentEntry?.title}</h1>
+            <h1 className="font-header text-5xl text-darkTertiary">
+              {currentEntry?.title}
+            </h1>
             <p className="text-darkTertiary">{currentEntry?.date}</p>
           </div>
           <p className="p-10 text-white">{currentEntry?.text}</p>
-          <button onClick={() => {
-            deleteEntry(currentEntry!.entryId!);
-            setModalIsOpen(false);
-            getEntriesByUserId(user!.user!.userId!);
-          }}
-          className="absolute bg-secondary text-white rounded-lg p-3 right-10 bottom-10"
+          <button
+            onClick={() => {
+              deleteEntry(currentEntry!.entryId!);
+              setModalIsOpen(false);
+              getEntriesByUserId(user!.user!.userId!);
+            }}
+            className="absolute bg-secondary text-white rounded-lg p-3 right-10 bottom-10"
           >
-            Delete</button>
-          <button></button>
+            Delete
+          </button>
+          <button onClick={() => {
+            setCurrentEntry(currentEntry);
+            setIsEditing(true);
+            setModalIsOpen(false);
+          }}>Update</button>
         </Modal>
       </div>
-      
     </div>
   );
 };
